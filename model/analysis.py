fix in river levels and adjusted regional gradient

model/analysis.py
@@ -21,12 +21,16 @@ izone_file = os.path.join('..','..','gis','idomain.tif')
 izone_rast = flopy.utils.Raster.load(izone_file)
 izone = izone_rast.resample_to_grid(ml.modelgrid, band=1, method='nearest')
 
+# load dtm raster
+dtm_file = os.path.join('..','..','gis','dtm_no_drn_ext_trim_filt.tif') 
+dtm_rast = flopy.utils.Raster.load(dtm_file)
+dtm = dtm_rast.resample_to_grid(ml.modelgrid, band=1, method='nearest')
+
 # load top (digital terrain elevation)
 top = ml.dis.top.data
 
-# load drain flow 
-cbc = ml.output.budget()
-drnb = ml.output.budget().get_data(text='DRN')
+# load swb 
+swb = pd.read_csv(os.path.join('swb_vars.csv'),index_col=0,parse_dates=True)
 
 # load head data 
 nper = sim.tdis.nper.data
@@ -50,47 +54,48 @@ ax.plot(hds0_2d[:, int(ml.dis.ncol.data/2)],label='ic')
 
 # head steady state cross-section
 
-hds_2d = hdfile.get_alldata()[0,0,:,:]
-ax.plot(hds_2d[:, int(ml.dis.ncol.data/2)],label='ss')
-fig.tight_layout()
+for n,i in enumerate(range(0,hds.shape[0],10)):
+    hds_2d = hds[i,0,:,:]
+    ax.plot(hds_2d[:, int(ml.dis.ncol.data/2)],linewidth=1,label='ss')
+    fig.tight_layout()
 
 # -------------------------------------
 #---  save steady state to shape file 
 # -------------------------------------
-
 head_shpfile = os.path.join('fig','heads_ss.shp')
 hdfile.to_shapefile(head_shpfile,kstpkper=(0,0))
+
 # -------------------------------------
 #---  maps of groundwater head
 # -------------------------------------
 
 # 2D map of gw heads 
-for n,i in enumerate(range(0,hds.shape[0],10)):
+for n,i in enumerate(range(0,hds.shape[0],1)):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(1, 1, 1, aspect="equal")
     modelmap = flopy.plot.PlotMapView(model=ml, ax=ax)
-    #ax.set_xlim(385600., 387500.)
+    ax.set_xlim(385600., 387500.)
     ax.set_title('Piézométrie du ' + dates_out[i].strftime("%d-%m-%Y"))
     # masked 2D head array
     hds2d = hds[i,0,:,:]
-    #hds2d[izone < 2]=np.nan
-    pa = modelmap.plot_array(hds2d, vmin=20,vmax=21)
+    hds2d[izone < 2]=np.nan
+    pa = modelmap.plot_array(hds2d, vmin=20.5,vmax=22)
     cb = plt.colorbar(pa, shrink=0.5)
     cb.set_label('m NGF')
     fig.savefig(os.path.join('fig',f'h_{n}.png'))
     plt.close()
 
-convert 'h_%d.png[0-274]' -scale 1066x800 -delay 20 -coalesce -layers Optimize -fuzz 2% +dither hmap.gif
+convert 'h_%d.png[0-264]' -scale 1066x800 -delay 20 -coalesce -layers Optimize -fuzz 2% +dither hmap.gif
 
 
 # -------------------------------------
 #---  map of groundwater depth
 # -------------------------------------
 
-gwd = top - hds
+gwd = dtm - hds
 
 # 2D map of gw depth 
-for n,i in enumerate(range(0,hds.shape[0],5)):
+for n,i in enumerate(range(0,hds.shape[0],1)):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(1, 1, 1, aspect="equal")
     modelmap = flopy.plot.PlotMapView(model=ml, ax=ax)
@@ -105,23 +110,30 @@ for n,i in enumerate(range(0,hds.shape[0],5)):
     plt.close()
 
 
-convert 'gwd_%d.png[0-54]' -scale 1066x800 -delay 20 -coalesce -layers Optimize -fuzz 2% +dither gwdmap.gif
+convert 'gwd_%d.png[0-264]' -scale 1066x800 -delay 20 -coalesce -layers Optimize -fuzz 2% +dither gwdmap.gif
 
 # -------------------------------------
 #--- drn records 
 # -------------------------------------
 
-drn_surf = 1071422 # m2
-drnf_records = -1*np.array([ drnb[i]['q'].sum() for i in range(nper)])/drn_surf*1000*86400 # m3/s to mm/d
+# load drain flow 
+zone_surf = ml.dis.delc[0]*ml.dis.delr[0]*(izone==2).sum()
+drnobs = pd.read_csv('sescousse.drn.obs.output.csv',index_col=0)
+drnobs.index = dates_out
+drnobs['flow']= drnobs.DRN*(-1/zone_surf*1000*86400) # m3/s to mm/d
+
+'''
+cbc = ml.output.budget()
+drnb = ml.output.budget().get_data(text='DRN')
+drnf_records = -1*np.array([ drnb[i]['q'].sum() for i in range(nper)])/zone_surf*1000*86400 # m3/s to mm/d
+'''
 
 fig,axs = plt.subplots(3,1,sharex=True, figsize=(10,6)) #A4 paper size
-
 ax0, ax1, ax2 = axs
-
-ax0.bar(clim.index,clim.D,color='darkgrey',label='Recharge')
+ax0.bar(swb.index,swb.R,color='darkgrey',label='Recharge')
 ax0.set_ylabel('mm/d')
-
-ax1.bar(hsim.date, drnf_records, color='tan',label='Drainage',alpha=0.8)
+#ax1.bar(dates_out, drnf_records, color='tan',label='Drainage CBC',alpha=0.8)
+ax1.bar(dates_out, drnobs.flow, color='tan',label='Drainage',alpha=0.8)
 ax1.set_ylabel('mm/d')
 
 for loc in ['PS1','PS2','PS3']:
@@ -136,9 +148,6 @@ lgd = [ax.legend(loc='upper left') for ax in [ax0,ax1,ax2]]
 fig.tight_layout()
 fig.savefig(os.path.join('fig','sim_records.pdf'),dpi=300)
 
-#
-
-'''
 # -------------------------------------
 #--- 3D surface plot 
 # -------------------------------------
@@ -147,24 +156,25 @@ X = ml.modelgrid.xcellcenters
 Y = ml.modelgrid.ycellcenters
 
 fig, ax = plt.subplots(1,1,figsize=(12,12),subplot_kw={"projection": "3d"})
-for i,n in enumerate(range(0,nper,5)):
+ax.view_init(elev=15., azim=-148)
+for i,n in enumerate(range(0,nper,1)):
     Z = hds[n,:,:][0,:,:]
     if i>0 : surf.remove()
     surf = ax.plot_surface(X, Y, Z, 
                     rstride=1, cstride=1, 
                     cmap=cm.viridis,
+                           vmin=20.5,vmax=22,
                     linewidth=0, antialiased=False)
-    ax.set_xlim(385600., 387500.)
-    ax.set_zlim(18,24)
+    ax.set_xlim(385800., 387500.)
+    ax.set_zlim(19,23)
     ax.set_title('Piézométrie du ' + hsim.date[n].strftime("%d-%m-%Y"))
     if i==0 : 
         cbar = fig.colorbar(surf, shrink=0.3, aspect=10)
         cbar.set_label('h [m NGF]')
     fig.savefig(os.path.join(sim_dir,'fig',f'hsurf_{i}.png'),dpi=128)
 
-#convert 'hsurf_%d.png[0-54]' -scale 1066x800 -delay 20 -coalesce -layers Optimize -fuzz 2% +dither hsurf.gif
+convert 'hsurf_%d.png[0-264]' -scale 1066x800 -delay 20 -coalesce -layers Optimize -fuzz 2% +dither hsurf.gif
 
-'''
 
 
 '''

model/ml_transient.py
@@ -6,7 +6,7 @@ import flopy
 import swb
 
 # --------------------------------------
-# ---- settings and data      ----------
+# ---- settings     ----------
 # --------------------------------------
 
 # steady model dir
@@ -26,25 +26,21 @@ start_date = pd.to_datetime('2023-10-15')
 end_date = pd.to_datetime('2024-07-06')
 sim_dates = pd.date_range(start_date,end_date) 
 
-# load river level recorded at FS4
+# --------------------------------------
+# --- load and pre-proc input data  
+# --------------------------------------
+
+# --- save observed levels over simulation period
 levels_file = os.path.join('..','data','levels_daily.csv')
 levels = pd.read_csv(levels_file,header=[0,1],index_col=0,parse_dates=True)
-riv_ref_records = levels.loc[start_date:end_date,('h','FS4')]
-
-# plot to think about the consistency of all that 
-df = levels.xs('h',axis=1)
-ax = df[['PS1','PS2','PS3','FS4']].plot()
-ax.set_xlim(start_date,end_date)
-ax.set_ylim(19.5,22.5)
-ax.axhline((17.2+21.9)/2,ls='--',color='grey')
+obs_locs = ['FS1','FS2','FS3','FS4','PS1','PS2','PS3']
+obs_levels = levels.xs('h',axis=1)[obs_locs]
+obs_levels.index = obs_levels.index.date
+obs_levels.to_csv(os.path.join('ml_transient','obs_levels.csv'))
 
 # load initial parameter values 
 pdata = pd.read_excel(os.path.join('..','data','par.xlsx'),index_col='name')
 parvals = pdata['val']
-
-# --------------------------------------
-# --- setup recharge model 
-# --------------------------------------
 
 # Weather data 
 stjean_file=os.path.join('..','data','stjean_daily.csv')
@@ -54,6 +50,14 @@ stjean_df = pd.read_csv(stjean_file, header=[0,1], index_col=0, parse_dates=True
 mf_file=os.path.join('..','data','ETP_daily.csv')
 mf_df = pd.read_csv(mf_file,parse_dates=True,header=[0],sep=';')
 mf_df.index = pd.to_datetime(mf_df.date,format='%d/%m/%Y')
+# subset to period of interest  
+mf_df = mf_df.loc[mf_df.index >= stjean_df.index.min()]
+
+# Piezometric records from ADES
+# https://ades.eaufrance.fr/Fiche/PtEau?code=07545X0029/F
+ades_file=os.path.join('..','data','BSS001VYWT.csv')
+ades_df = pd.read_csv(ades_file,header=[0],sep=',')
+ades_df.index = pd.to_datetime(ades_df.date,format='%d/%m/%Y')
 
 # subset 
 P = stjean_df.loc[sim_dates,('Rain_mm_Tot','sum')].values
@@ -62,6 +66,80 @@ PET = mf_df.loc[sim_dates,'ETP'].values
 # save to single clim file 
 clim_df = pd.DataFrame({'P':P,'PET':PET},index=sim_dates)
 clim_df.to_csv(os.path.join(transient_dir,'clim.csv'))
+
+# --------------------------------------
+# ---- plot input data     ----------
+# --------------------------------------
+
+fig,axs = plt.subplots(3,1,sharex=True, figsize=(10,6)) #A4 paper size
+
+ax0, ax1, ax2 = axs
+
+ax0.bar(stjean_df.index, stjean_df.xs('Rain_mm_Tot',axis=1)['sum'], color='darkblue', label='P')
+ax0.bar(mf_df.index, mf_df.ETP, color='tan', label='PET',alpha=0.8)
+ax0.set_ylabel('mm/d')
+ax0.legend(loc='upper left')
+
+for pid in ['FS1','FS2','FS3','FS4']:
+    obs_levels[pid].plot(ax=ax1,label=pid)
+
+ax1.set_ylabel('m NGF')
+ax1.legend(loc='upper right')
+
+
+for pid in ['PS1','PS2','PS3']:
+    obs_levels[pid].plot(ax=ax2,label=pid)
+
+ax2.set_ylabel('m NGF')
+ax2.legend(loc='upper right')
+
+date_min, date_max = ax0.get_xlim()
+date_min = pd.to_datetime('2023-07-01')
+date_max = pd.to_datetime('2024-08-01')
+ax0.set_xlim(date_min,date_max)
+
+ax2.legend(loc='lower right')
+
+fig.align_ylabels()
+fig.tight_layout()
+
+fig.savefig(os.path.join('fig','obs_vars.pdf'),dpi=300)
+
+
+# -----------------------------------------------------------
+# ---- investigate regional hydraulic gradient     ----------
+# -----------------------------------------------------------
+
+
+bheads_df = pd.merge( obs_levels[['PS1','PS2']] ,  ades_df['F'], 
+                     left_index=True,right_index=True, 
+                     how='left')
+
+fig,axs = plt.subplots(3,1,sharex=True, figsize=(10,6)) #A4 paper size
+ax0, ax1, ax2 = axs
+
+bheads_df[['PS1','PS2','F']].plot(ax=ax0)
+
+bheads_df['PS1m'] = bheads_df.PS1 - bheads_df.PS1.mean()
+bheads_df['PS2m'] = bheads_df.PS2 - bheads_df.PS2.mean()
+bheads_df['Fm'] = bheads_df.F - bheads_df.F.mean()
+
+bheads_df[['PS1m','PS2m','Fm']].plot(ax=ax1)
+
+dist_PS1_F = 5300 # m 
+bheads_df['gradh'] = (bheads_df.PS1 - bheads_df.F) /dist_PS1_F
+bheads_df['gradh'].plot(ax=ax2)
+ax2.set_ylim(0.0005,0.0007)
+
+fig.align_ylabels()
+fig.tight_layout()
+
+fig.savefig(os.path.join('fig','gradh.pdf'),dpi=300)
+
+
+# --------------------------------------
+# --- setup recharge model 
+# --------------------------------------
 
 swb.run_swb(theta_sat = parvals.loc['tsat'],
             D_max= parvals.loc['dmax'],
@@ -130,13 +208,17 @@ driv_rec0['cond'] = parvals.loc['criv']
 
 # reference level
 riv_ref_value = 20.6 # min(h_fs4(t))
+
+# river level records at FS4
+riv_ref_records = levels.loc[start_date:end_date,('h','FS4')]
+
 # fluctuations to reference level
 riv_dh = riv_ref_records.values - riv_ref_value
 
 # gen stress period data 
-driv_rec = driv_rec0.copy()
 driv_spd = {}
 for i in range(nper):
+    driv_rec = driv_rec0.copy()
     driv_rec['elev'] = driv_rec0['elev']+riv_dh[i]
     driv_spd[i]=driv_rec
 
@@ -151,10 +233,23 @@ driv = flopy.mf6.ModflowGwfdrn(ml,
 ml.remove_package('riv')
 
 # --- ghb package 
-#ghb = ml.get_package('ghb_0')
-#ghb_rec0 = ghb.stress_period_data.get_data()[0]
-#ghb_rec0['cond'] = parvals.loc['cghb']
-#ml.ghb.stress_period_data.set_data({0:ghb_rec0})
+
+ghb = ml.get_package('ghb_0')
+ghb_rec0 = ghb.stress_period_data.get_data()[0]
+
+# fluctuations to reference level
+ghb_dh = ades_df.F - ades_df.loc[start_date,'F'] # start_date at low flow
+ghb_dh = ghb_dh.reindex(sim_dates) # re-index with simulation dates
+dhb_dh = ghb_dh.interpolate() # gap filling by linear interpolation over simulation period
+
+# gen stress period data 
+ghb_spd = {}
+for i in range(nper):
+    ghb_rec = ghb_rec0.copy()
+    ghb_rec['bhead'] = ghb_rec0['bhead']+ghb_dh[i]
+    driv_spd[i]=ghb_rec
+
+ml.ghb.stress_period_data.set_data({0:ghb_rec0})
 
 # --- oc package 
 oc = ml.get_package('oc')
@@ -179,12 +274,7 @@ ss_hdata = ss_head.get_alldata()[0]
 # replace initial condition with pre-computed steady state
 ml.ic.strt.set_data(ss_hdata)  
 
-# --- save observed levels over simulation period
-obs_locs = ['FS1','FS2','FS3','FS4','PS1','PS2','PS3']
-obs_levels = levels.xs('h',axis=1)[obs_locs]
-obs_levels.index = obs_levels.index.date
-obs_levels = obs_levels.loc[start_date:end_date]
-obs_levels.to_csv(os.path.join('ml_transient','obs_levels.csv'))
+
 
 # -- write simulation
 sim.set_sim_path(transient_dir)