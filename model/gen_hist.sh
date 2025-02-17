6b72d652a79e53c6af50a9256a12ef105a59d68b
Alexandre Pryet <apryet@manzana.geoenv.ipb.fr>
2025-02-17 12:31:13 +0100

    minor edits

--- model/gen_hist.sh
+++ model/gen_hist.sh
@@ -1,30 +1,17 @@
 #!/bin/zsh
+
+python gen_hist.py --tpl_dir=drn0 --sim_dir=histo_drn0 > histo_drn0.out &
+python gen_hist.py --tpl_dir=drn40 --sim_dir=histo_drn40 > histo_drn40.out &
+python gen_hist.py --tpl_dir=drn110 --sim_dir=histo_drn110 > histo_drn110.out &
+python gen_hist.py --tpl_dir=drn150 --sim_dir=histo_drn150 > histo_drn150.out &
+python gen_hist.py --tpl_dir=drn200 --sim_dir=histo_drn200 > histo_drn200.out &
+
 python gen_hist.py --tpl_dir=drn_alt_ld_40 --sim_dir=histo_drn_alt_ld_40 > histo_drn_alt_ld_40.out &
 python gen_hist.py --tpl_dir=drn_alt_ld_110 --sim_dir=histo_drn_alt_ld_110 > histo_drn_alt_ld_110.out &
 python gen_hist.py --tpl_dir=drn_alt_hd_40 --sim_dir=histo_drn_alt_hd_40 > histo_drn_alt_hd_40.out &
 python gen_hist.py --tpl_dir=drn_alt_hd_110 --sim_dir=histo_drn_alt_hd_110 > histo_drn_alt_hd_110.out &
 
 
-cd histo_drn_alt_dd_40 
-python forward_run.py &
-cd ..
-
-cd histo_drn_alt_dd_110 
-python forward_run.py &
-cd ..
-
-cd histo_drn_alt_hd_40 
-python forward_run.py &
-cd ..
-
-cd histo_drn_alt_hd_110
-python forward_run.py &
-cd ..
-
-/home/apryet/model/sescousse/sim/histo
-
-
-
-for i in {1..1}; do
+for i in {1..11}; do
     python gen_prosp.py --tpl_dir=drn110 --cm=${i} > prosp_$(printf "%02d" "$i").out &
 
